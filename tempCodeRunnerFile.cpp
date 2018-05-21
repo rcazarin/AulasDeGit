1d4011449f753bea109582fb76988dadc25da301
rcazarin <rcazarin@gmail.com>
2018-05-21 19:32:12 -0300

    ultima alteracao

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main ()
+{
+    char nome[10];
+    cout << "Escreva um nome";
+    scanf("%s", &nome);
+    cout << nome << endl;
+    system("sleep 10");
+    return (0);
+}
--- teste2.cpp
+++ teste2.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+int main ()
+{
+    char nome[10];
+    printf("Escreva um nome: ");
+    scanf("%c", &nome);
+    printf("%s\n\n", nome);
+    system("sleep 10");
+    return (0);
+}
